ea5b9fa4ceaff7057d4713d59d6d7051a7fe1b17
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2024-12-29 13:30:22 +0530

    added new neovim config

---
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+      require("telescope").setup {
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          }
+        }
+      }
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+    end
+  }
+}
