202094b5803ecad20a503150440d7b1b14223592
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2025-01-30 22:16:40 +0530

    config: add terminal remap

--- .config/nvim/lua/config/plugins/toggleterm.lua
+++ .config/nvim/lua/config/plugins/toggleterm.lua
@@ -6,6 +6,12 @@ return {
       require('toggleterm').setup {
         open_mapping = [[<C-\>]]
       }
+    function _G.set_terminal_keymaps()
+      local opts = {buffer = 0}
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    end
+
+    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
     end
   }
 }
---
+++ .config/nvim/lua/remap.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { buffer = 0, silent = true })
